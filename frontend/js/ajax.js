3b4b972eb7885f5ccaf9a0d3fb7e1a486b37babd
lunavod <lunavod@bk.ru>
2017-04-29 09:57:48 +0300

    Fixed despoil Close #129

--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -23,9 +23,9 @@ export function ajax(url, params, callback, more) {
         url: url,
         data: params,
         dataType: more.dataType || 'json',
-        success: callback || function () {
-            Tools.debug("ajax success: ");
-            Tools.debug.apply(this, arguments);
+        success: callback? function(){console.log("Test!");callback.apply(this,arguments)}: function () {
+            console.log("ajax success: ");
+            console.log.apply(this, arguments);
         }.bind(this),
         error: more.error || function (msg) {
             Tools.debug("ajax error: ");
@@ -35,27 +35,6 @@ export function ajax(url, params, callback, more) {
             Tools.debug("ajax complete: ");
             Tools.debug.apply(this, arguments);
         }.bind(this),
-	xhr: function()
-	{
-		var xhr = new window.XMLHttpRequest();
-		// прогресс загрузки на сервер
-		xhr.upload.addEventListener("progress", function(evt){
-			if (evt.lengthComputable) {
-				var percentComplete = evt.loaded / evt.total;
-				// делать что-то...
-				console.log("Complete upload: ",evt.loaded, evt.total);
-			}
-		}, false);
-		// прогресс скачивания с сервера
-		xhr.addEventListener("progress", function(evt){
-			if (evt.lengthComputable) {
-				var percentComplete = evt.loaded / evt.total;
-				// делать что-то...
-				console.log("Complete xhr: ",evt.loaded, evt.total);
-			}
-		}, false);
-		return xhr;
-	},
     };
 
     Emitter.emit('ls_ajax_before', [ajaxOptions], this);

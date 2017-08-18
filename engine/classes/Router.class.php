fc32159878b5d75c5b12619f0f05d696964416f9
silvman <anoscillator@gmail.com>
2017-08-19 00:28:30 +0300

    Фикс для ссылок почтовика

--- engine/classes/Router.class.php
+++ engine/classes/Router.class.php
@@ -459,7 +459,7 @@ class Router extends LsObject {
 
 		// Смотрим, есть ли правило rewrite
 		$sPage = self::getInstance()->Rewrite($sPage);
-		return "/$sPage/";
+		return rtrim(Config::Get('path.root.web'),'/')."/$sPage/";
 	}
 	/**
 	 * Try to find rewrite rule for given page.

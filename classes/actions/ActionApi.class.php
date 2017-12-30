64e5edb1196c1bedc719172ecaae08251df1e092
lunavod <lunavod@bk.ru>
2017-12-30 16:23:54 +0300

    Test api login

---
+++ classes/actions/ActionApi.class.php
@@ -0,0 +1,130 @@
+<?php
+/*-------------------------------------------------------
+*
+*   LiveStreet Engine Social Networking
+*   Copyright © 2008 Mzhelskiy Maxim
+*
+*--------------------------------------------------------
+*
+*   Official site: www.livestreet.ru
+*   Contact e-mail: rus.engine@gmail.com
+*
+*   GNU General Public License, version 2:
+*   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*
+---------------------------------------------------------
+*/
+
+class ActionApi extends Action
+
+{
+    /**
+     * Текущий пользователь
+     *
+     * @var ModuleUser_EntityUser|null
+     */
+    protected $oUserCurrent = null;
+
+    /**
+     * Инициализация
+     */
+    public
+
+    function Init()
+    {
+    }
+
+    /**
+     * Регистрация евентов
+     */
+    protected
+    function RegisterEvent()
+    {
+        $this->AddEvent('login', 'EventAjaxLogin');
+    }
+
+    /**********************************************************************************
+     ************************ РЕАЛИЗАЦИЯ ЭКШЕНА ***************************************
+     **********************************************************************************
+     */
+
+    protected
+    function EventTest()
+    {
+        echo "TEST";
+    }
+    protected function EventAjaxLogin() {
+
+        //Проверяем тип запроса. Если не POST - возвращаем ошибку 400
+        $this->Viewer_SetResponseAjax('json',true,false);
+        if ($_SERVER['REQUEST_METHOD'] != "POST") {
+            $this->Viewer_AssignAjax('message', 'Bad request');
+            return;
+        }
+
+        if ((getRequest('login') and getRequest('password')) or getRequest('device_uid')) {
+
+            //Если логин и пароль не заданы, то пробуем залогинить по device_uid
+
+            if (!getRequest('login') and !getRequest('password') and getRequest('device_uid')){
+                if ($oUser=$this->User_GetUserById($this->ModuleApi_getUserByKey(getRequest('device_uid')))) {
+                    if ($this->User_Authorization($oUser,true)){
+                        $this->Viewer_AssignAjax('notice', "Authenfication complete!");
+                    }
+                } else {
+                    $this->Viewer_AssignAjax('message', 'Device UID incorrect');
+                    return;
+                }
+                return;
+            }
+
+            if (!is_string(getRequest('login')) or !is_string(getRequest('password'))) {
+                $this->Viewer_AssignAjax('message', "Login or password isn't a string");
+                return;
+            }
+            /**
+             * Проверяем есть ли такой юзер по логину
+             */
+            if ((func_check(getRequest('login'),'mail') and $oUser=$this->User_GetUserByMail(getRequest('login')))  or  $oUser=$this->User_GetUserByLogin(getRequest('login'))) {
+                /**
+                 * Сверяем хеши паролей и проверяем активен ли юзер
+                 */
+                // ежели пароль - несолёный md5, но совпадает, обновим его, и сделаем вид, что так и было
+                if ( strlen($oUser->getPassword()) == 32 && $oUser->getPassword() == func_encrypt(getRequest('password')) )  {
+                    $oUser->setPassword(encryptPassword(getRequest('password')));
+                    $this->User_Update($oUser);
+                }
+                if (checkPassword($oUser->getPassword(), getRequest('password'))) {
+                    if (!$oUser->getActivate()) {
+                        $this->Viewer_AssignAjax('message', "Login or password isn't a string");
+                        return;
+                    }
+                    /**
+                     * Авторизуем
+                     */
+                    if ($this->User_Authorization($oUser,true)){
+                        $aResult["notice"] = "Authenfication complete!";
+                        if (getRequest('device_uid')){
+                            if ($sKey = $this->ModuleApi_setKey($oUser->getId(), getRequest('device_uid'))){
+                                $this->Viewer_AssignAjax('newKey', $sKey);
+                            }
+                        }
+                    } else {
+                        $this->Viewer_AssignAjax('message', 'Authenfication faild');
+                        return;
+                    }
+                } else {
+                    $this->Viewer_AssignAjax('message', 'Wrong password!');
+                    return;
+                }
+
+            } else {
+                $this->Viewer_AssignAjax('message', 'User not exists!');
+                return;
+            }
+        } else {
+            $this->Viewer_AssignAjax('message', 'Bad request');
+            return;
+        }
+    }
+}
---
+++ classes/modules/api/mapper/Api.mapper.class.php
@@ -0,0 +1,43 @@
+<?php
+
+class PluginApi_ModuleApi_MapperApi extends Mapper
+{
+    public function writeKey($iId, $sKey){
+        $sql = "INSERT INTO prefix_api_keys (user_id, `uid`) VALUES(?, ?)";
+        $iId=$this->oDb->query($sql, $iId, $sKey);
+        return true;
+
+    }
+    public function readKey($iId){
+        $sql = "SELECT `uid` FROM prefix_api_keys WHERE user_id = ?";
+        if ($aRows=$this->oDb->query($sql, $iId)){
+            return $aRows[0]['key'];
+        } else {
+            return false;
+        }
+
+    }
+
+    public function deleteKey($sKey){
+        $sql = "DELETE FROM prefix_api_keys WHERE uid = ?";
+        if ($this->oDb->query($sql, $sKey)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function getUserByKey($sKey){
+        $sql = "SELECT user_id FROM prefix_api_keys WHERE `uid` = ?";
+        if ($aRows=$this->oDb->query($sql, $sKey)){
+            return $aRows[0]['user_id'];
+        } else {
+            return false;
+        }
+
+    }
+
+}
+
+?>

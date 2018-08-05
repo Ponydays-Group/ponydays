d9c3c84ca14ef9b9d470ac6ea3b2ea3537e54bf8
mineozelot <mineozelot@gmail.com>
2018-08-06 03:49:57 +0700

    Revert "Фекс регулярки призыва"
    This reverts commit 1468d08

--- classes/modules/comment/Comment.class.php
+++ classes/modules/comment/Comment.class.php
@@ -489,7 +489,7 @@ class ModuleComment extends Module {
             $oComment->setId($sId);
             $oTarget = $this->Topic_GetTopicById($oComment->getTargetId());
             $sTextOld = $oComment->getText();
-            $sText = preg_replace_callback('/@(.+?)\((.+?)\)/',
+            $sText = preg_replace_callback('/@(.*?)\((.*?)\)/',
                 function ($matches) use ($oComment) {
                     $sLogin = $matches[1];
                     $sNick = $matches[2];
@@ -501,9 +501,8 @@ class ModuleComment extends Module {
                     }
                     return $matches[0];
                 }, $sTextOld);
-            $sText = preg_replace_callback('/@(.+?)/',
+            $sText = preg_replace_callback('/@([a-zA-Zа-яА-Я0-9-_]+)/',
                 function ($matches) use ($oComment) {
-            		var_dump($matches);
                     $sLogin = $matches[1];
                     $r = "<a href=\"/profile/" . $sLogin . "/\" class=\"summon-user\">&#64;" . $sLogin . "</a>";
                     if ($oTargetUser = $this->User_getUserByLogin($sLogin)) {

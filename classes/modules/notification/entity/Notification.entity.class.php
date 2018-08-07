9e62aba8af9c450b37afbcc8df10cc701d9775b6
frumscepend <tolyas111@gmail.com>
2018-08-08 00:18:03 +0700

    Созданы сущности и мапперы.

---
+++ classes/modules/notification/entity/Notification.entity.class.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frumscepend
+ * Date: 7/30/18
+ * Time: 1:52 AM
+ */
+
+/**
+ * Объект сущности уведомлений
+ *
+ * @package modules.notification
+ */
+class ModuleNotification_EntityNotification extends Entity {
+
+	/**
+	 * Возвращает ID уведомления
+	 *
+	 * @return int|null
+	 */
+	public function getId() {
+		return $this->_getDataOne('notification_id');
+	}
+
+	/**
+	 * Возвращает ID пользователя
+	 *
+	 * @return int|null
+	 */
+	public function getUserId() {
+		return $this->_getDataOne('user_id');
+	}
+
+	/**
+	 * Возвращает дату уведомления
+	 *
+	 * @return string|null
+	 */
+	public function getDate() {
+		return $this->_getDataOne('date');
+	}
+
+	/**
+	 * Возвращает текст уведомления
+	 *
+	 * @return string|null
+	 */
+	public function getText() {
+		return $this->_getDataOne('text');
+	}
+
+	/**
+	 * Возвращает заголовок уведомления
+	 *
+	 * @return string|null
+	 */
+	public function getTitle() {
+		return $this->_getDataOne('title');
+	}
+
+	/**
+	 * Возвращает ссылку уведомления
+	 *
+	 * @return string|null
+	 */
+	public function getLink() {
+		return $this->_getDataOne('link');
+	}
+
+	/**
+	 * Возвращает оценку
+	 *
+	 * @return int|null
+	 */
+	public function getRating() {
+		return $this->_getDataOne('rating');
+	}
+
+	/**
+	 * Возвращает тип уведомления
+	 *
+	 * @return int|null
+	 */
+	public function getType() {
+		return $this->_getDataOne('notification_type');
+	}
+
+	/**
+	 * Устанавливает ID уведомления
+	 *
+	 * @param int $data
+	 */
+	public function setId($data) {
+		$this->_aData['notification_id']=$data;
+	}
+
+	/**
+	 * Устанавливает ID пользователя
+	 *
+	 * @param int $data
+	 */
+	public function setUserId($data) {
+		$this->_aData['user_id']=$data;
+	}
+
+	/**
+	 * Устанавливает дату уведомления
+	 *
+	 * @param string $data
+	 */
+	public function setDate($data) {
+		$this->_aData['date']=$data;
+	}
+
+	/**
+	 * Устанавливает текст уведомления
+	 *
+	 * @param string $data
+	 */
+	public function setText($data) {
+		$this->_aData['text']=$data;
+	}
+
+	/**
+	 * Устанавливает заголовок уведомления
+	 *
+	 * @param string $data
+	 */
+	public function setTitle($data) {
+		$this->_aData['title']=$data;
+	}
+
+	/**
+	 * Устанавливает ссылку уведомления
+	 *
+	 * @param string $data
+	 */
+	public function setLink($data) {
+		$this->_aData['link']=$data;
+	}
+
+	/**
+	 * Устанавливает оценку пользователя
+	 *
+	 * @param int $data
+	 */
+	public function setRating($data) {
+		$this->_aData['rating']=$data;
+	}
+
+	/**
+	 * Устанавливает тип уведомления
+	 *
+	 * @param int $data
+	 */
+	public function setType($data) {
+		$this->_aData['notification_type']=$data;
+	}
+}
+?>
---
+++ classes/modules/notification/mapper/Notification.mapper.class.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frumscepend
+ * Date: 7/30/18
+ * Time: 1:53 AM
+ */
+
+/**
+ * Маппер уведомлений, работа с базой данных
+ *
+ * @package modules.notification
+ */
+
+class ModuleNotification_MapperNotification extends Mapper {
+
+	/**
+	 * Получение всех уведомлений пользователя с пагинацией
+	 * @param $userId int ID пользователя
+	 * @param $page int номер страницы
+	 * @param $count int количество на странице
+	 * @return array уведомления
+	 * @throws Exception
+	 */
+
+	public function getNotification($userId, $page, $count){
+		$sql = "SELECT
+				*
+				FROM
+				".Config::Get('db.table.notification')."
+				WHERE
+				user_id = ?
+				LIMIT ? 
+				OFFSET ?
+		";
+		$aNotifications=array();
+		if ($aRows=$this->oDb->select($sql,$userId, $count, (($page-1)*$count))) {
+			foreach ($aRows as $aRow) {
+				$aNotifications[]=Engine::GetEntity('Notification',$aRow);
+			}
+		}
+		return $aNotifications;
+	}
+
+	/**
+	 * Создание нового уведомления
+	 *
+	 * @param ModuleNotification_EntityNotification $eNotification
+	 * @return bool|int
+	 */
+	public function createNotification(ModuleNotification_EntityNotification $eNotification){
+		$sql = "INSERT INTO ".Config::Get('db.table.notification')."
+				(user_id          
+				date             
+				text             
+				title            
+				link             
+				rating           
+				notification_type)
+				VALUES (?, ?, ?, ?, ?, ?, ?)
+		";
+		if ($iId=$this->oDb->query($sql,$eNotification->getUserId(), $eNotification->getDate(), $eNotification->getText(),
+			$eNotification->getTitle(), $eNotification->getLink(), $eNotification->getRating(), $eNotification->getType())) {
+        	return $iId;
+		}
+		return false;
+	}
+
+	/**
+	 * Удаление уведомления
+	 *
+	 * @param $notificationId
+	 * @return bool
+	 */
+	public function deleteNotification($notificationId) {
+		$sql = "DELETE FROM ".Config::Get('db.table.notification')." 
+				WHERE notification_id = ?d";
+		if ($this->oDb->query($sql,$notificationId)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Удаление всех уведомлений пользователя
+	 * @param $userId
+	 * @return bool
+	 */
+	public function deleteAllNotifications($userId) {
+		$sql = "DELETE FROM ".Config::Get('db.table.notification')." 
+				WHERE user_id = ?d";
+		if ($this->oDb->query($sql,$userId)) {
+			return true;
+		}
+		return false;
+	}
+
+	//todo: удаление по массиву id, удаление по типу
+
+}
+?>

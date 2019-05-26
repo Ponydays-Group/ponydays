1c07320de1a10e2553bd7c9f407c98ed59bfb7d3
mineozelot <mineozelot@gmail.com>
2019-05-26 15:13:00 +0700

    Комментарии к миграции

--- database/migration/m_19_523_user_privileges.sql
+++ database/migration/m_19_523_user_privileges.sql
@@ -1,5 +1,8 @@
 /**
- * Создает битовый столбец для хранения прав пользователя
+ * Создает битовый столбец для хранения прав пользователя.
+ * Измените это поле для пользователей из config(moderator) и config(quotes_admin) соответствующе.
+ * Значения битов описаны в классе ModuleUser.
+ * Уберите поля config(moderator) и config(quotes_admin) из конфига после, так как они бесполезны в дальнейшем.
  */
 
 ALTER TABLE prefix_user ADD COLUMN user_perms BIT(8) NOT NULL DEFAULT b'00000000';
